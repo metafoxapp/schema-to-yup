59890a7c1c7ffece405c42968b3902516d1241b8
Nam Nguyen <namnv@younetgroup.com>
2022-02-21 09:14:18 +0700

    fix: add commonjs

--- babel.config.js
+++ babel.config.js
@@ -6,12 +6,9 @@ module.exports = {
       {
         targets: {
           node: "current",
-          edge: "17",
-          firefox: "60",
-          chrome: "67",
-          safari: "11.1",
         },
       },
     ],
   ],
+  plugins: ["@babel/plugin-proposal-nullish-coalescing-operator"],
 };
